098e1fb7c16a4cf5a9787655a4b240f34404b9fe
Fernando Flugel Assad <63256562+ferflugel@users.noreply.github.com>
2021-02-13 16:22:38 -0300

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,8 +1,13 @@
 import discord, matplotlib.pyplot as plt, numpy as np
+import random
 from math import *
+from discord.ext import commands
 
 client = discord.Client()
 
+intents = discord.Intents.all()
+intents.members = True  # Subscribe to the privileged members intent.
+client = commands.Bot(command_prefix='~', intents=intents)
 # testing server id: 810149199488352256
 
 @client.event
@@ -12,6 +17,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
   id = client.get_guild(810149199488352256)
+  guild = client.get_guild(810149199488352256)
   
   if message.author == client.user:
     return
@@ -23,5 +29,29 @@ async def on_message(message):
   if message.content.startswith('~announcement'):
     await message.channel.purge(limit=1)
     await message.channel.send(f"@everyone\n```diff\n+Announcement\n{message.content[13:]}```")
+
+  # ASSEMBLE STUDENTS AT THE SAME CLASSROOM
+  if message.content.startswith('~assemble'):
+    channel = client.get_channel(810226322625265704) 
+    member = message.author
+    for member in guild.members:
+      try:
+        await member.move_to(channel)
+      except:
+        pass
+  
+  # PUT STUDENTS INTO BREAKOUT ROOMS
+  if message.content.startswith('~breakout'):
+    for member in guild.members:
+      if random.random() < 0.3333:
+        channel = client.get_channel(810214075912421427) 
+      elif random.random() < 0.6666:
+        channel = client.get_channel(810214372592713759)
+      else:
+        channel = client.get_channel(810214406636961854)
+      try:
+        await member.move_to(channel)
+      except:
+        pass
 
 client.run('Token Goes Here')
