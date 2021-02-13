2ccb5c130fab151c0d276f1526594d75964bdc1a
Fernando Flugel Assad <63256562+ferflugel@users.noreply.github.com>
2021-02-13 18:46:27 -0300

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -14,6 +14,9 @@ client = commands.Bot(command_prefix='~', intents=intents)
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+questions = []
+q = ['']
+
 @client.event
 async def on_message(message):
   id = client.get_guild(810149199488352256)
@@ -28,7 +31,7 @@ async def on_message(message):
   # MAKING AN ANNOUNCEMENT
   if message.content.startswith('~announcement'):
     await message.channel.purge(limit=1)
-    await message.channel.send(f"@everyone\n```diff\n+Announcement\n{message.content[13:]}```")
+    await message.channel.send(f"@everyone\n```diff\n+Announcement\n{message.content[14:]}```")
 
   # ASSEMBLE STUDENTS AT THE SAME CLASSROOM
   if message.content.startswith('~assemble'):
@@ -69,5 +72,22 @@ async def on_message(message):
         await member.edit(mute=False)
       except:
         pass
+
+  # CREATES A QUESTION DATABASE
+  if message.content.startswith('~question'):
+    questions.append(message.content[10:])
+    questions.append(message.author)
+
+  # SHOWS THE QUESTIONS 
+  if message.content.startswith('~show_questions'):
+    q[0] = ''
+    for i in range(0, len(questions) - 1, 2):
+      q[0] += f"{str(int(i/2 + 1))}: {questions[i]} | {questions[i+1]}\n"
+    await message.channel.send(f"```{q[0]}```")
+
+  # ANSWERS A QUESTION
+  if message.content.startswith('~answer'):
+    index = (int(message.content[8]) - 1) * 2
+    await message.channel.send(f"{questions[index + 1].mention} here is the answer to **{questions[index] message.content[8]}**: ")
 
 client.run('Token Goes Here')
