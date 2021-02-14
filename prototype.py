27b800d786a4eed7664e353b534e60e1945cb4b8
Fernando Flugel Assad <63256562+ferflugel@users.noreply.github.com>
2021-02-13 21:24:11 -0300

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 client = discord.Client()
 
 intents = discord.Intents.all()
-intents.members = True  # Subscribe to the privileged members intent.
+intents.members = True  
 client = commands.Bot(command_prefix='~', intents=intents)
 # testing server id: 810149199488352256
 
@@ -21,10 +21,13 @@ q = ['']
 async def on_message(message):
   id = client.get_guild(810149199488352256)
   guild = client.get_guild(810149199488352256)
-  
+
   if message.author == client.user:
     return
 
+  ########################## BASIC FUNCTIONALITIES ##############################
+
+  # SAYING HI
   if message.content.startswith('~hi'):
     await message.channel.send("hey")
   
@@ -34,39 +37,42 @@ async def on_message(message):
     await message.channel.send(f"@everyone\n```diff\n+Announcement\n{message.content[14:]}```")
 
   # ASSEMBLE STUDENTS AT THE SAME CLASSROOM
-  if message.content.startswith('~assemble'):
+  if message.content.startswith('~assemble') and 'Professor' in str(message.author.roles):
     channel = client.get_channel(810226322625265704) 
     member = message.author
     for member in guild.members:
-      try:
-        await member.move_to(channel)
-      except:
-        pass
+      if 'Student' in str(member.roles):
+        try:
+          await member.move_to(channel)
+        except:
+          pass
   
   # PUT STUDENTS INTO BREAKOUT ROOMS
-  if message.content.startswith('~breakout'):
+  if message.content.startswith('~breakout') and 'Professor' in str(message.author.roles):
     for member in guild.members:
-      if random.random() < 0.3333:
-        channel = client.get_channel(810214075912421427) 
-      elif random.random() < 0.6666:
-        channel = client.get_channel(810214372592713759)
-      else:
-        channel = client.get_channel(810214406636961854)
-      try:
-        await member.move_to(channel)
-      except:
-        pass
+      if 'Student' in str(member.roles):
+        if random.random() < 0.3333:
+          channel = client.get_channel(810214075912421427) 
+        elif random.random() < 0.6666:
+          channel = client.get_channel(810214372592713759)
+        else:
+          channel = client.get_channel(810214406636961854)
+        try:
+          await member.move_to(channel)
+        except:
+          pass
   
-  # MUTES EVERY MEMBER
-  if message.content.startswith('~mute'):
+  # MUTES EVERY STUDENT
+  if message.content.startswith('~mute') and 'Professor' in str(message.author.roles):
     for member in guild.members:
-      try:
-        await member.edit(mute=True)
-      except:
-        pass
+      if 'Student' in str(member.roles):
+        try:
+          await member.edit(mute=True)
+        except:
+          pass
 
   # UNMUTES EVERY MEMBER
-  if message.content.startswith('~unmute'):
+  if message.content.startswith('~unmute') and 'Professor' in str(message.author.roles):
     for member in guild.members:
       try:
         await member.edit(mute=False)
@@ -92,9 +98,53 @@ async def on_message(message):
     questions.pop(index)
     questions.pop(index)
 
-# CREATE A POLL
+  # CREATE A POLL
   if message.content.startswith('~poll'):
     await message.add_reaction('👍')
     await message.add_reaction('👎')
+
+  ########################## INTEGRATED CALCULATOR ##############################
+
+  # ADDITION
+  if message.content.startswith('~add'):
+    a, b = int(message.content.split()[1]), int(message.content.split()[2])
+    await message.channel.send(str(a + b))
+
+  # SUBTRACTION
+  if message.content.startswith('~sub'):
+    a, b = int(message.content.split()[1]), int(message.content.split()[2])
+    await message.channel.send(str(a - b))
+
+  # MULTIPLICATION
+  if message.content.startswith('~mult'):
+    a, b = int(message.content.split()[1]), int(message.content.split()[2])
+    await message.channel.send(str(a * b))
+
+  # DIVISION
+  if message.content.startswith('~div'):
+    a, b = int(message.content.split()[1]), int(message.content.split()[2])
+    await message.channel.send(str(a / b))
+
+  # MARK
+  if message.content.startswith('~mark'):
+    a, b = int(message.content.split()[1]), int(message.content.split()[2])
+    await message.channel.send(f"{str(round(100 * (a / b), 2))}%")
+
+  # SQUARE ROOT 
+  if message.content.startswith('~sqrt'):
+    a = int(message.content.split()[1])
+    await message.channel.send(f"{str(a ** (1/2))}")
+
+  # POWER 
+  if message.content.startswith('~pow'):
+    a, b = int(message.content.split()[1]), int(message.content.split()[2])
+    await message.channel.send(str(a ** b))
+
+  # AVERAGE
+  if message.content.startswith('~avg'):
+    total = 0;
+    for number in message.content.split()[1:]:
+      total += int(number)
+    await message.channel.send(str(total / len(message.content.split()[1:])))
 
 client.run('Token Goes Here')
